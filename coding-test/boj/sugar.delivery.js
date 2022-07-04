2d793d33dac77b2713ce486eafabc168124a19be
rgh <rheegon@gmail.com>
2022-07-04 18:01:15 +0900

    feat(coding-test): 백준 greedy 문제 풀이

--- coding-test/boj/sugar.delivery.js
+++ coding-test/boj/sugar.delivery.js
@@ -4,26 +4,28 @@
  * @returns {number} 봉지의 수
  */
 
-function solution(n) {
-  const INFINITE = 10000;
-  let x = INFINITE,
-    y = INFINITE;
+const fs = require('fs');
+const n = +fs.readFileSync('/dev/stdin').toString().trim();
 
-  for (let _5의몫 = Math.floor(n / 5); _5의몫 >= 0; _5의몫 -= 1) {
-    if ((n - 5 * _5의몫) % 3 === 0) {
-      const _x = _5의몫;
-      const _y = (n - 5 * _5의몫) / 3;
+const INFINITE = 10000;
+let x = INFINITE,
+  y = INFINITE;
 
-      if (x + y > _x + _y) {
-        x = _x;
-        y = _y;
-      }
+for (let _5의몫 = Math.floor(n / 5); _5의몫 >= 0; _5의몫 -= 1) {
+  if ((n - 5 * _5의몫) % 3 === 0) {
+    const _x = _5의몫;
+    const _y = (n - 5 * _5의몫) / 3;
+
+    if (x + y > _x + _y) {
+      x = _x;
+      y = _y;
     }
   }
-
-  if (x + y > INFINITE) {
-    return -1;
-  }
-
-  return x + y;
 }
+
+if (x + y > INFINITE) {
+  console.log(-1);
+  return;
+}
+
+console.log(x + y);
