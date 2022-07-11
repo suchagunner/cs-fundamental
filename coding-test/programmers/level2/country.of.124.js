540e186d0c17b62ee0e5713ec2944e85f411fdfb
rgh <rheegon@gmail.com>
2022-07-11 22:25:24 +0900

    feat(coding-test): 프로그래머스 구현 문제 풀이추가

---
+++ coding-test/programmers/level2/country.of.124.js
@@ -0,0 +1,23 @@
+function solution(n) {
+  const REMAINS = {
+    0: 4,
+    1: 1,
+    2: 2,
+  };
+
+  var answer = '';
+  let number = n;
+
+  while (number > 0) {
+    const remains = number % 3;
+    answer += REMAINS[remains];
+
+    if (remains === 0) {
+      number = Math.floor((number - 1) / 3);
+    } else {
+      number = Math.floor(number / 3);
+    }
+  }
+
+  return answer;
+}
